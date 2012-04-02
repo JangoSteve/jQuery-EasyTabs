e6308d5a4d815a6ff607c664bae7db317c2ca32f
Steve Schwartz <steve@alfajango.com>
2012-04-02 15:33:03 -0400

    Added pending specs for things that need tests added

--- test/spec/EasyTabsSpec.js
+++ test/spec/EasyTabsSpec.js
@@ -14,21 +14,108 @@ describe('easytabs', function() {
       $("#tab-container").remove();
     });
 
-    it('should be easytabs container', function () {
+    it('is easytabs container', function () {
       expect(fixture).toBeEasyTabs();
     });
 
-    it('should have tabs', function() {
+    it('has tabs', function() {
       expect(fixture).toHaveTabs('[href="#tabs1-html"]');
       expect(fixture).toHaveTabs('[href="#tabs1-html"]');
       expect(fixture).toHaveTabs('[href="#tabs1-js"]');
       expect(fixture).toHaveTabs('[href="#tabs1-css"]');
     });
 
-    it('should have panels', function() {
+    it('has panels', function() {
       expect(fixture).toHavePanels('#tabs1-html');
       expect(fixture).toHavePanels('#tabs1-js');
       expect(fixture).toHavePanels('#tabs1-css');
+    });
+
+    xit('makes first tab the default', function() {
+    });
+
+    xit('makes all but default panel hidden', function() {
+    });
+
+    xit('makes default tab active', function() {
+    });
+
+    xit('updates url hash', function() {
+    });
+
+    xit('selects tab when url hash is updated', function() {
+    });
+
+    xit('makes tab from url hash active instead of default tab', function() {
+    });
+
+  });
+
+  describe('disconnected tabs and panels', function() {
+    xit('loads tabs', function() {
+    });
+
+    xit('loads disconnected panels', function() {
+    });
+  });
+
+  describe('using non-div panels', function() {
+    xit('loads form sections into panels', function() {
+    });
+  });
+
+  describe('animating tab change', function() {
+
+    describe('transitions', function() {
+    });
+
+  });
+
+  describe('events', function() {
+    xit('fires easytabs:before event', function() {
+    });
+
+    xit('fires easytabs:midTransition event', function() {
+    });
+
+    xit('fires easytabs:after event', function() {
+    });
+
+    xit('cancels easytabs:before event', function() {
+    });
+  });
+
+  describe('uiTabs', function() {
+    xit('gives tabs proper classes', function() {
+    });
+
+    xit('gives panels proper classes', function() {
+    });
+  });
+
+  describe('cycling tabs', function() {
+  });
+
+  describe('public methods', function() {
+  });
+
+  describe('collapsible', function() {
+  });
+
+  describe('ajax tabs', function() {
+
+    describe('events', function() {
+      xit('fires easytabs:ajax:beforeSend event', function() {
+      });
+
+      xit('fires easytabs:ajax:complete event', function() {
+      });
+
+      xit('cancels easytabs:ajax:beforeSend event', function() {
+      });
+    });
+
+    describe('caching panels', function() {
     });
 
   });
